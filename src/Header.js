89cba3ef3c4f2c8b733ab157ea4dc75b77c3f2ae
jimniDev <cy_dy@naver.com>
2023-04-19 10:38:39 +0900

    FEAT: Header Layout

--- src/Header.js
+++ src/Header.js
@@ -1,16 +1,23 @@
-import { Link } from 'react-router-dom';
+import { Link, useMatch } from 'react-router-dom';
 import styles from "./Header.module.css";
+import classNames from 'classnames/bind';
 
 export function Header() {
+    const cx = classNames.bind(styles);
+    const todayMatched = useMatch("/");
+    const archiveMatched = useMatch("/archive");
+
     return (
         <div className={styles.header}>
             <div className={styles.logo}></div>
             <div className={styles.nav}>
-                <Link to={'/'}>Today</Link>
-                <Link to={'/archive'}>Archive</Link>
+                <Link to={'/'} className={cx('today', {active : todayMatched ? true : false})}>Today</Link>
+                <label> | </label>
+                <Link to={'/archive'} className={cx('archive', {active : archiveMatched ? true : false})}>Archive</Link>
             </div>
             <div className={styles.userInfo}>
-                <p>Jimin Kim</p>
+                <img className={styles.userImg} src='./img/Spotify_Logo_CMYK_Black.png'></img>
+                <p className={styles.userName}>Jimin Kim</p>
             </div>
         </div>
     );
