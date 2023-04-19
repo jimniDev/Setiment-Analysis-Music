aff5aa713a3b3efe39ee8de3d9bb7550e827ae86
jimniDev <cy_dy@naver.com>
2023-04-19 13:52:22 +0900

    FEAT: Current Date Design Modify

--- src/CurrentDate.js
+++ src/CurrentDate.js
@@ -24,15 +24,16 @@ export function CurrentDate() {
                         padding: `20px`,
                         display: `inline-block`,
                         filter: `drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.1))`,
+                        fontFamily: `Poppins`,
             }}>
             <div className="month"
-                style={{ fontWeight: `800`, fontSize: `30px`, lineHeight: `100.6%`, textTransform: `capitalize`}}
+                style={{ fontWeight: `800`, fontSize: `30px`, lineHeight: `100%`, textTransform: `capitalize`}}
                 >{monthNames[month]}</div>
             <div className="date"
-                style={{ fontWeight: `700`, fontSize: `60px`, lineHeight: `90.6%`, textTransform: `capitalize`}}
+                style={{ fontWeight: `700`, fontSize: `65px`, lineHeight: `100%`, textTransform: `capitalize`}}
                 >{date}</div>
             <div className="year"
-                style={{ fontWeight: `800`, fontSize: `30px`, lineHeight: `130.6%`, textTransform: `capitalize`}}
+                style={{ fontWeight: `800`, fontSize: `30px`, lineHeight: `100%`, textTransform: `capitalize`}}
                 >{year}</div>
         </div>
     )
